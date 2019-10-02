15fba3def81fc0d5692e09fc44ab436d95d39f1a
David Dixon <david@digitaloasis.co.uk>
2019-10-02 23:06:33 +0100

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ echo "${OUTPUT}"
 if [ "${CODE}" -ne "0" ]; then
     snyk test --json $* | snyk-to-html -o results.html
     echo ::set-output name=results::results.html
-    exit 1
 fi
+
+exit $CODE
