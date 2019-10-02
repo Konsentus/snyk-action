1db63124f84ed3f7fde910e6fb92e942ac83c8f4
David Dixon <david@digitaloasis.co.uk>
2019-10-02 20:28:26 +0100

    additional changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 OUTPUT=$(snyk test $*)
-CODE=${PIPESTATUS[0]}
+CODE=$?
 
 echo "${OUTPUT}"
 
 if (( ${CODE} )); then
     snyk test --json $* | snyk-to-html -o results.html
     echo ::set-output name=results::results.html
+    exit 1
 fi
