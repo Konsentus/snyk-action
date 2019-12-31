ca7bb94c99c5b8218cfb1ac4d9a164ab2a8e5a93
David Dixon <david.dixon@konsentus.com>
2019-12-31 00:43:32 +0000

    something not quite right, testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -l
 
-OUTPUT=$(snyk test $*)
+echo "snyk test ${INPUT_OPTIONS} $*"
+OUTPUT=$(snyk test ${INPUT_OPTIONS} $*)
 CODE=$?
 
 echo "${OUTPUT}"
 
 if [ "${CODE}" -ne "0" ]; then
+    echo "snyk test ${INPUT_OPTIONS} --json $* | snyk-to-html -o results.html"
     snyk test ${INPUT_OPTIONS} --json $* | snyk-to-html -o results.html
     echo ::set-output name=results::results.html
 fi
