97ac9f0f26c5db6f1c2e772bd6a8c4571f307956
David Dixon <david.dixon@konsentus.com>
2019-12-31 00:38:08 +0000

    allow passing of additional options to snyk tool

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 OUTPUT=$(snyk test $*)
 CODE=$?
+OPTIONS=${$INPUT_OPTIONS:-}
 
 echo "${OUTPUT}"
 
 if [ "${CODE}" -ne "0" ]; then
-    snyk test --json $* | snyk-to-html -o results.html
+    snyk test ${OPTIONS} --json $* | snyk-to-html -o results.html
     echo ::set-output name=results::results.html
 fi
 
-exit $CODE
+exit ${CODE}
