884565ba25a172452a48ccb3bf1aa1be70bd9309
David Dixon <david.dixon@konsentus.com>
2019-12-31 00:45:56 +0000

    looks like snyk doesnt follow standards when using input params. fixing on requsting workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh -l
 
-echo "snyk test ${INPUT_OPTIONS} $*"
 OUTPUT=$(snyk test ${INPUT_OPTIONS} $*)
 CODE=$?
 
 echo "${OUTPUT}"
 
 if [ "${CODE}" -ne "0" ]; then
-    echo "snyk test ${INPUT_OPTIONS} --json $* | snyk-to-html -o results.html"
     snyk test ${INPUT_OPTIONS} --json $* | snyk-to-html -o results.html
     echo ::set-output name=results::results.html
 fi
