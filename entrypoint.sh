510156f8e1e42267ce48dbf8bad8686f8723a598
David Dixon <david.dixon@konsentus.com>
2020-03-30 19:08:03 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "${INPUT_IGNORE}" ]; then
-    echo "${INPUT_IGNORE}" | jq -c '.[]' | while read i; do
+    echo "${INPUT_IGNORE}" | jq -r '.[]' | while read i; do
         snyk ignore --id=${i} --reason="Ignored by workflow" --expiry="$(date -d '+1 hour' --iso-8601=minutes)"
     done
 fi
